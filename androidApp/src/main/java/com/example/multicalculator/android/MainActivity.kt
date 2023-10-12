4594e1e6929f9b6014a04caf2c4c22fe2d1b286e
KoushikMadugula <A00288822@mycambrian.ca>
2023-10-12 18:58:37 -0400

    CalcEqualsButton

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.multicalculator.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -65,6 +68,13 @@ fun CalcOperationButton(){
 }
 
 @Composable
-fun CalcEqualsButton(){
-
+fun CalcEqualsButton(display: MutableState<String>){
+    Button(
+        onClick = {
+            display.value = "0"
+    },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = "=")
+    }
 }
