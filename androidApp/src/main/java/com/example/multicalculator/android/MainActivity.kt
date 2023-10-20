f165dd0c7db5f1570c132d8db4bf8759c12c3049
KoushikMadugula <A00288822@mycambrian.ca>
2023-10-20 18:29:36 -0400

    commit 1

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -16,8 +16,11 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -45,7 +48,22 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun CalcView(){
-    val displayText = remember { mutableStateOf("0") }
+    var leftNumber by rememberSaveable { mutableStateOf(0) }
+    var rightNumber by rememberSaveable { mutableStateOf(0) }
+    var operation by rememberSaveable { mutableStateOf("") }
+    var complete by rememberSaveable { mutableStateOf(false) }
+    var displayText by rememberSaveable { mutableStateOf("0") }
+
+    if (complete && operation != "") {
+        var answer by rememberSaveable { mutableStateOf(0) }
+        when (operation){
+            "+" -> answer = leftNumber + rightNumber
+            "-" -> answer = leftNumber - rightNumber
+            "*" -> answer = leftNumber * rightNumber
+            "/" -> answer = leftNumber / rightNumber
+        }
+        displayText = answer.toString()
+    }
 
     Column (modifier = Modifier.background(Color.LightGray)
         ) {
